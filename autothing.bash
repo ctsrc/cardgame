9570e9e0c2bf6b9730580479ebf9bd48762ceb9c
Erik Nordstrøm <erik@nordstroem.no>
2016-03-03 00:19:29 +0100

    Use valgrind when running klondike from the autothing. Not for memory leaks since we don't currently malloc but for quick insights into other errors.

--- autothing.bash
+++ autothing.bash
@@ -3,11 +3,12 @@
 # Automatically rebuild and run klondike when source files change.
 # Depends on inotifywait, which is part of inotify-tools on
 # FreeBSD 10.2, Fedora 23 and Debian GNU/Linux 8.
+# Depends on valgrind.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 while true; do
   inotifywait -r -e modify,attrib,close_write,move,create,delete \
     "$DIR/src" 2>/dev/null 1>&2 && reset && make \
-    && ./build/$( cc -dumpmachine )/debug/klondike -d 3
+    && valgrind -v ./build/$( cc -dumpmachine )/debug/klondike -d 3
 done
