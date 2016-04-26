ce0fe395b3613dd1185034d995720d5e0017ec2c
Erik Nordstrøm <erik@nordstroem.no>
2016-04-26 22:58:56 +0200

    Implement proper cards.

--- src/server/klondike.scm
+++ src/server/klondike.scm
@@ -24,6 +24,17 @@
 		(C (list-tail l (+ b 1))))
 		(append A (cons (list-ref l b) B) (cons (list-ref l a) C))))
 
+(define (n-to-card n)
+	(let ((rank (modulo n 13)))
+		(let ((color (modulo (- n rank) 4)))
+			(list
+				(cond ((= color 0) 'hearts)
+					((= color 1) 'spades)
+					((= color 2) 'diamonds)
+					((= color 3) 'clubs))
+				(+ rank 1)
+				#f))))
+
 ; Inside-out Fisher-Yates
 (define (shuffled-deck)
 	((define (fill-deck d i)
@@ -32,10 +43,18 @@
 			(fill-deck
 				(let ((j (arc4random_uniform (+ i 1))))
 					(if (= i j)
-						(cons i d)
-						(swap-index 0 (- i j) (cons i d))))
+						(cons (n-to-card i) d)
+						(swap-index 0 (- i j)
+							(cons (n-to-card i) d))))
 				(+ i 1))))
 		'() 0))
 
-(define deck (shuffled-deck))
+(define (unshuffled-deck)
+	((define (fill-deck d i)
+		(if (= 52 i)
+			d
+			(fill-deck (cons (n-to-card i) d) (+ i 1))))
+		'() 0))
+
+(define deck (unshuffled-deck))
 (print (length deck) "\n" deck)
