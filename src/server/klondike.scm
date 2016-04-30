48e54fac62d8e03b28127b0188602121bf8df368
Erik Nordstrøm <erik@nordstroem.no>
2016-04-30 22:28:32 +0200

    Whitespace.

--- src/server/klondike.scm
+++ src/server/klondike.scm
@@ -21,134 +21,132 @@
 (bind "uint32_t arc4random_uniform (uint32_t upper_bound)")
 
 (define (swap-index a b l)
-	(let ((A (take l a))
-		(B (take (list-tail l (+ a 1)) (- b (+ a 1))))
-		(C (list-tail l (+ b 1))))
-		(append A (cons (list-ref l b) B) (cons (list-ref l a) C))))
+  (let ((A (take l a))
+        (B (take (list-tail l (+ a 1)) (- b (+ a 1))))
+        (C (list-tail l (+ b 1))))
+    (append A (cons (list-ref l b) B) (cons (list-ref l a) C))))
 
 (define (card color rank facing-up)
-	(list 'card color rank facing-up))
+  (list 'card color rank facing-up))
 
 (define (card? l)
-	(eq? (car l) 'card))
+  (eq? (car l) 'card))
 
 (define (card-color card)
-	(cadr card))
+  (cadr card))
 
 (define (card-rank card)
-	(caddr card))
+  (caddr card))
 
 (define (card-facing-up? card)
-	(cadddr card))
+  (cadddr card))
 
 (define (n-to-card n)
-	(let* ((rank (modulo n 13))
-		(color (modulo (- n rank) 4)))
-		(list
-			'card
-			(cond ((= color 0) 'hearts)
-				((= color 1) 'spades)
-				((= color 2) 'diamonds)
-				((= color 3) 'clubs))
-			(+ rank 1)
-			#f)))
+  (let* ((rank (modulo n 13))
+         (color (modulo (- n rank) 4)))
+    (list 'card
+          (cond ((= color 0) 'hearts)
+                ((= color 1) 'spades)
+                ((= color 2) 'diamonds)
+                ((= color 3) 'clubs))
+          (+ rank 1)
+          #f)))
 
 (define (cards l)
-	(cdr l))
+  (cdr l))
 
 (define (deck cards)
-	(cons 'deck cards))
+  (cons 'deck cards))
 
 (define (deck? l)
-	(eq? (car l) 'deck))
+  (eq? (car l) 'deck))
 
 (define (insert-no-shuffle i deck)
-	(cons (n-to-card i) deck))
+  (cons (n-to-card i) deck))
 
 ; Inside-out Fisher-Yates
 (define (insert-shuffle i deck)
-	(let ((j (arc4random_uniform (+ i 1))))
-		(if (= i j)
-			(insert-no-shuffle i deck)
-			(swap-index 0 (- i j) (insert-no-shuffle i deck)))))
+  (let ((j (arc4random_uniform (+ i 1))))
+    (if (= i j)
+      (insert-no-shuffle i deck)
+      (swap-index 0 (- i j) (insert-no-shuffle i deck)))))
 
 (define (init-deck deck-inserter)
-	(deck ((define (fill-deck deck i)
-		(if (= 52 i)
-			deck
-			(fill-deck (deck-inserter i deck) (+ i 1))))
-		'() 0)))
+  (deck ((define (fill-deck deck i)
+           (if (= 52 i)
+             deck
+             (fill-deck (deck-inserter i deck) (+ i 1))))
+         '() 0)))
 
 (define (waste cards)
-	(cons 'waste cards))
+  (cons 'waste cards))
 
 (define (waste? l)
-	(eq? (car l) 'waste))
+  (eq? (car l) 'waste))
 
 (define (foundation cards)
-	(cons 'foundation cards))
+  (cons 'foundation cards))
 
 (define (foundation? l)
-	(eq? (car l) 'foundation))
+  (eq? (car l) 'foundation))
 
 (define (tableau cards)
-	(cons 'tableau cards))
+  (cons 'tableau cards))
 
 (define (tableau? l)
-	(eq? (car l) 'tableau))
+  (eq? (car l) 'tableau))
 
 (define (hand origin held)
-	(list 'hand origin held))
+  (list 'hand origin held))
 
 (define (hand-origin hand)
-	(cadr hand))
+  (cadr hand))
 
 (define (hand-held hand)
-	(caddr hand))
+  (caddr hand))
 
 (define (foundation-accept-hand? target hand)
-	(and (eq? (cdr (hand-held hand)) '())
-		(or (eq? (cards target) '())
-			(let ((fct (car (cards target)))
-				(fch (car (hand-held hand))))
-				(and (eq? (card-color fch) (card-color fct))
-					(= (card-rank fch)
-						(+ (card-rank fct) 1)))))))
+  (and (eq? (cdr (hand-held hand)) '())
+       (or (eq? (cards target) '())
+           (let ((fct (car (cards target)))
+                 (fch (car (hand-held hand))))
+             (and (eq? (card-color fch) (card-color fct))
+                  (= (card-rank fch) (+ (card-rank fct) 1)))))))
 
 (define (tableau-accept-hand? target hand)
-	(or (eq? (cards target) '())
-		(let ((fct (car (cards target)))
-			(fch (car (hand-held hand))))
-			(and (or (and (or (eq? (card-color fch) 'hearts)
-					(eq? (card-color fch) 'diamonds))
-				(or (eq? (card-color fct) 'hearts)
-					(eq? card-color fch) 'diamonds))
-				(and (or (eq? (card-color fch) 'spades)
-						(eq? (card-color fch) 'clubs))
-					(or (eq? (card-color fct) 'spades)
-						(eq? card-color fch) 'clubs)))
-				(= (card-rank fch) (+ (card-rank fct) 1))))))
+  (or (eq? (cards target) '())
+      (let ((fct (car (cards target)))
+            (fch (car (hand-held hand))))
+        (and (or (and (or (eq? (card-color fch) 'hearts)
+                          (eq? (card-color fch) 'diamonds))
+                      (or (eq? (card-color fct) 'hearts)
+                          (eq? card-color fch) 'diamonds))
+                 (and (or (eq? (card-color fch) 'spades)
+                          (eq? (card-color fch) 'clubs))
+                      (or (eq? (card-color fct) 'spades)
+                          (eq? card-color fch) 'clubs)))
+             (= (card-rank fch) (+ (card-rank fct) 1))))))
 
 (define (accept-hand? target hand)
-	(cond ((waste? target) (deck? (hand-origin hand)))
-		((foundation? target) (foundation-accept-hand? target hand))
-		((tableau? target) (tableau-accept-hand? target hand))))
+  (cond ((waste? target) (deck? (hand-origin hand)))
+        ((foundation? target) (foundation-accept-hand? target hand))
+        ((tableau? target) (tableau-accept-hand? target hand))))
 
 (if DEBUG (print "Running DEBUG build."))
 
 (if DEBUG (define no-shuffle-deck #t))
 
 (if (and DEBUG no-shuffle-deck)
-	(print "No shuffle deck. (Available to DEBUG build only.)"))
+  (print "No shuffle deck. (Available to DEBUG build only.)"))
 
 (define current-deck
-	(if (and DEBUG no-shuffle-deck)
-		(init-deck insert-no-shuffle)
-		(init-deck insert-shuffle)))
+  (if (and DEBUG no-shuffle-deck)
+    (init-deck insert-no-shuffle)
+    (init-deck insert-shuffle)))
 
 (if DEBUG
-	(begin (print (deck? current-deck) " "
-		(length (cards current-deck)) "\n" current-deck)
-		(let ((top (cadr current-deck)))
-			(print (card? top) " " (card-color top) " "
-				(card-rank top) " " (card-facing-up? top)))))
+  (begin (print (deck? current-deck) " "
+                (length (cards current-deck)) "\n" current-deck)
+         (let ((top (cadr current-deck)))
+           (print (card? top) " " (card-color top) " "
+                  (card-rank top) " " (card-facing-up? top)))))
