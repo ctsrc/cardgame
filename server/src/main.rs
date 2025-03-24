8ba583f6b9250114df15c1322b888b0b4b1b58df
Erik Nordstrøm <erik@nordstroem.no>
2025-03-24 01:38:24 +0100

    Rearrange some code slightly in server main.

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,16 +12,16 @@ fn main() {
     // Wire representation of what we would send to client if we were to send deck at this point.
     let deck_for_client: WireShuffledDeckServerOrigin = deck.to_owned().into();
 
-    // Display cards in deck on server-side representation.
-    println!("{deck}");
-
-    println!();
-
-    // Debug display wire repr of what we would send to client regarding cards in deck.
+    // `Debug` display wire repr of what we would send to client regarding cards in deck.
     dbg!(&deck_for_client);
 
     println!();
 
-    // Debug display cards in deck on server-side representation.
+    // `Debug` display cards in deck on server-side representation.
     dbg!(&deck);
+
+    println!();
+
+    // `Display` cards in deck on server-side representation.
+    println!("{deck}");
 }
